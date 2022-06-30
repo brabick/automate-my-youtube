6a5d85f9c3516425817a5600dd93a008e9403825
Brandon <brabick@gmail.com>
2022-06-30 13:33:47 -0600

    Progress on the xml writing

--- Project/PremiereXMLParser.py
+++ Project/PremiereXMLParser.py
@@ -6,7 +6,6 @@ import subprocess
 import uuid
 
 
-
 class XMLParser:
 
     video_duration = 0
@@ -15,7 +14,7 @@ class XMLParser:
     base_duration = 5000
     base_name = 'Albert Einstein'
     body_sections = []
-
+    images_xml_path = 'C:/Automated YouTube Project/FreeMoney/Project/images/'
 
     def write_header_xml(self):
 
@@ -32,26 +31,41 @@ class XMLParser:
         directory = os.listdir('F:/Video_Resources/Albert_Ein/images/')
         clip_id = 1
         clip_start = 0
+
         with open('Testing_XML_Writing/track.xml') as f:
             content = f.read()
             parsed_xml = BeautifulSoup(content, 'html.parser')
             tag = parsed_xml.clipitem['id']
             #print(tag)
             #print(parsed_xml)
-            for file in directory:
-                file_xml = parsed_xml
-                file_xml.find(text='%%_name_%%').replace_with(file)
-                print(file_xml)
-                file_xml.find(text='%%_clip_duration_%%').replace_with(str(self.base_duration / len(directory)))
-                tag = file_xml.clipitem
-                #print(tag)
-                file_xml.find(text='%%_clip_id_%%').replace_with(str(clip_id))
-                file_xml.find(text='%%_clip_start_%%').replace_with(str(clip_start))
-                clip_start += (self.base_duration / len(directory))
-                file_xml.find(text='%%_clip_end_%%').replace_with(str(clip_start))
-                #print(file_xml)
+            if not os.path.exists(self.images_xml_path):
+                os.makedirs(self.images_xml_path)
+        for file in directory:
+
+            file_xml = BeautifulSoup(content, 'html.parser')
+            file_xml.find(text='%%_name_%%').replace_with(file)
+            file_xml.find(text='%%_clip_duration_%%').replace_with(str(self.base_duration / len(directory)))
+
+            clip_item_tag = file_xml.clipitem
+            master_clip_id_tag = file_xml.masterclipid
+            file_id_tag = file_xml.file
+            print(file_id_tag)
+            clip_item_tag['id'] = "clipitem-" + str(clip_id)
+            master_clip_id_tag['id'] = "masterclip-" + str(clip_id)
+            file_id_tag['id'] = "file-" + str(clip_id)
+
+            file_xml.find(text='%%_clip_start_%%').replace_with(str(clip_start))
+            clip_start += (self.base_duration / len(directory))
+            file_xml.find(text='%%_clip_end_%%').replace_with(str(clip_start))
+            #print(file_xml)
+
+            f = open(self.images_xml_path + file[0:-4] + '.txt', 'w')
+            f.write(str(file_xml))
+            f.close()
+            clip_id += 1
 
 
+        #print(self.body_sections)
 
     def get_length(self, filename):
         """
